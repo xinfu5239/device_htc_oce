7b82c8007d0c8736a2a565483e2beaf70b9dfc2c
Rashed Abdel-Tawab <rashed@linux.com>
2018-07-25 11:21:07 -0700

    oce: Import USB HAL from hardware/interfaces
    Copied from 01787f47f4ee024889c13ec3a2b30624f6d5745d
    
    Change-Id: I85c41fa0844adea68336a368de3bec1f5a60d3d9

--- device.mk
+++ device.mk
@@ -530,7 +530,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.oce
 
 # Vibrator
 PRODUCT_PACKAGES += \
