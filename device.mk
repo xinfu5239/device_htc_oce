15d42d095ef3c0372a1292e61c019ffae01a1476
TARKZiM <tom8476oo@gmail.com>
2018-10-20 13:39:00 +0800

    oce: build DeviceSettings

--- device.mk
+++ device.mk
@@ -169,8 +169,8 @@ PRODUCT_PACKAGES += \
     services-ext
 
 # Device Settings
-#PRODUCT_PACKAGES += \
-#    DeviceSettings
+PRODUCT_PACKAGES += \
+    DeviceSettings
 
 # Display
 PRODUCT_PACKAGES += \
