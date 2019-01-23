74e6dadec59fe0307cbb8586d44eb26e19272696
TARKZiM <tom8476oo@gmail.com>
2019-01-23 10:36:19 +0800

    RR-ify

--- device.mk
+++ device.mk
@@ -24,7 +24,7 @@ $(call inherit-product-if-exists, vendor/htc/oce/oce-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage \
-#    $(LOCAL_PATH)/overlay-rr
+    $(LOCAL_PATH)/overlay-rr
 
 PRODUCT_ENFORCE_RRO_TARGETS := framework-res
 
