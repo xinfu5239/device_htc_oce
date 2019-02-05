7a84a0dfa4b7025dfbcede1a62ec0cd5a6069a28
TARKZiM <tom8476oo@gmail.com>
2019-02-05 21:20:03 +0800

    oce: sepolicy: updates for P * import from https://github.com/Siluxsept/device_htc_oce

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -226,8 +226,9 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_RIL_VARIANT := caf
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+-include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # SHIMS
 TARGET_LD_SHIM_LIBS := \
