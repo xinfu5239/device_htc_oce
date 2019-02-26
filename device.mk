6ce715bcd1c537eb22b33c466a9f27704adc435f
TARKZiM <tom8476oo@gmail.com>
2019-02-26 14:36:42 +0800

    oce: transform power hal name to oce

--- device.mk
+++ device.mk
@@ -402,7 +402,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.pme
+    android.hardware.power@1.2-service.oce
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
