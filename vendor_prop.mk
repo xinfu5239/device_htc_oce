7bb52874bf46b96e432840467bbbad4731aa4c63
TARKZiM <tom8476oo@gmail.com>
2019-03-15 23:04:35 +0800

    Revert "oce: Add QCOM's WFD implementation"
    This reverts commit 76f1860ce12b5a89b1d625889bcdeb73a00ae78a.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -211,9 +211,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # WFD
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1 \
-    vendor.gralloc.disable_wb_ubwc=1 \
-    persist.sys.wfd.nohdcp=1 \
-    debug.sf.enable_hwc_vds=1
+    persist.sys.wfd.virtual=0
 
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
