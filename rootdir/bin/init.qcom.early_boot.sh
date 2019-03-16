6166af9203deda8e4983ef9b0f1fd02145d1444c
TARKZiM <tom8476oo@gmail.com>
2019-03-16 10:09:06 +0800

    Revert "oce: kang Power-off alarm blobs from tissot pie"
    This reverts commit 797c8fd8db260830bc6120aa31fdf87f3c629783.

--- rootdir/bin/init.qcom.early_boot.sh
+++ rootdir/bin/init.qcom.early_boot.sh
@@ -386,10 +386,15 @@ fi
 
 boot_reason=`cat /proc/sys/kernel/boot_reason`
 reboot_reason=`getprop ro.boot.alarmboot`
+power_off_alarm_file=`cat /persist/alarm/powerOffAlarmSet`
 if [ "$boot_reason" = "3" ] || [ "$reboot_reason" = "true" ]; then
-    setprop ro.vendor.alarm_boot true
+    if [ "$power_off_alarm_file" = "1" ]
+    then
+        setprop ro.alarm_boot true
+        setprop debug.sf.nobootanimation 1
+    fi
 else
-    setprop ro.vendor.alarm_boot false
+    setprop ro.alarm_boot false
 fi
 
 # copy GPU frequencies to system property
