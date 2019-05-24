894dac06f769cc74db81853213c83f178a118f2f
TARKZiM <tom8476oo@gmail.com>
2019-05-24 11:46:59 +0800

    Revert "Revert "oce: Add QCOM's WFD implementation""
    This reverts commit 24dd3a2bb914bd6ba13fd2667fd4ecd3772a84e7.

--- device.mk
+++ device.mk
@@ -118,6 +118,8 @@ PRODUCT_PACKAGES += \
     audio.primary.msm8996 \
     audio.r_submix.default \
     audio.usb.default \
+    libaacwrapper \
+    libnl \
     libaudio-resampler \
     libaudiopolicymanager \
     libqcompostprocbundle \
@@ -516,6 +518,10 @@ PRODUCT_PACKAGES += \
 # VNDK-SP:
 PRODUCT_PACKAGES += \
     vndk-sp
+
+# Wifi-Display
+PRODUCT_BOOT_JARS += \
+    WfdCommon
 
 # Wifi
 PRODUCT_PACKAGES += \
