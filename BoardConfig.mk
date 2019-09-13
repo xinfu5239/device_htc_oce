16344e257349ca9a1efd49efd9961c290be9f4c6
TARKZiM <tom8476oo@gmail.com>
2019-09-13 19:53:33 +0800

    oce: Add minimal sepolicy for Q
    * Disable symlink for dsp.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,7 +98,7 @@ USE_CUSTOM_AUDIO_POLICY := 0
 USE_XML_AUDIO_POLICY_CONF := 1
 TARGET_USES_QCOM_MM_AUDIO := true
 
-BOARD_ROOT_EXTRA_SYMLINKS := /vendor/lib/dsp:/dsp
+#BOARD_ROOT_EXTRA_SYMLINKS := /vendor/lib/dsp:/dsp
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
@@ -219,6 +219,7 @@ TARGET_USES_OLD_MNC_FORMAT := true
 -include device/qcom/sepolicy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 #BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-minimal
 
 # SHIMS
 TARGET_LD_SHIM_LIBS := \
