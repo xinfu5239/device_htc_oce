a690bd16f71f189de958db039a6066aede7e38ff
Davide Garberi <dade.garberi@gmail.com>
2019-09-16 14:50:46 +0200

    oce: Import biometrics interface from android-10.0.0_r2
    Change-Id: I42aec640e146b54a433886460e7002ed3b6a248e

--- device.mk
+++ device.mk
@@ -226,7 +226,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service.oce
 
 # FlipFlap
 PRODUCT_PACKAGES += \
