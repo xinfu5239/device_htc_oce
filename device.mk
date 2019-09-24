d3dc31d29256d3b97947c5a731ef45ec0a4e60d3
TARKZiM <tom8476oo@gmail.com>
2019-09-24 11:02:42 +0800

    Revert "oce: audio HIDL HAL updates"
    This reverts commit 2d3ac3d30104bfea44d692689ed36b7fc0f62f7f.

--- device.mk
+++ device.mk
@@ -124,11 +124,9 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.common@4.0 \
     android.hardware.audio.common@4.0-util \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
     android.hardware.audio.effect@4.0 \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
-    android.hardware.soundtrigger@2.1-service
+    android.hardware.soundtrigger@2.1-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
