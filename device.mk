7ba39da1faba6019bd2ac43983382826f0c39351
TARKZiM <tom8476oo@gmail.com>
2019-10-01 11:29:15 +0800

    oce: releasetools: Add new variant scripts
    * Use /sbin/toybox directly for copying blobs.
    * Copy blobs from new folder structure.

--- device.mk
+++ device.mk
@@ -538,3 +538,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/calibration:$(TARGET_COPY_OUT_VENDOR)/etc/calibration \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
+# Variant linking script
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/releasetools/variant_script.sh:install/bin/variant_script.sh
