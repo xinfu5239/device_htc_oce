ad8482f15e3b9c7fe352c46aa3e164d0736a0535
TARKZiM <tom8476oo@gmail.com>
2019-10-01 11:29:15 +0800

    oce: releasetools: Add new variant scripts
    * Use /sbin/toybox directly for copying blobs.
    * Copy blobs from new folder structure.

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -14,18 +14,6 @@
 #
 
 def FullOTA_InstallEnd(info):
-  info.script.AppendExtra('if (getprop("ro.boot.mid") == "2PZF10000") then')
-  info.script.Print("This is a uhl device - renaming radio props")
-  info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/bootdevice/by-name/system", "/system", "");')
-  info.script.AppendExtra('rename("/system/vendor/lib64/libril-qc-ltedirectdisc.so.uhl", "/system/vendor/lib64/libril-qc-ltedirectdisc.so");')
-  info.script.AppendExtra('rename("/system/vendor/lib64/libril-qc-qmi-1.so.uhl", "/system/vendor/lib64/libril-qc-qmi-1.so");')
-  info.script.AppendExtra('rename("/system/vendor/lib64/libril-qc-radioconfig.so.uhl", "/system/vendor/lib64/libril-qc-radioconfig.so");')
-  info.script.AppendExtra('rename("/system/vendor/lib64/libril-qcril-hook-oem.so.uhl", "/system/vendor/lib64/libril-qcril-hook-oem.so");')
-  info.script.AppendExtra('unmount("/system");')
-  info.script.AppendExtra('endif;')
-  info.script.AppendExtra('if (getprop("ro.boot.mid") == "2PZF30000") then')
-  info.script.Print("This is a dtwl device - renaming radio props")
-  info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/bootdevice/by-name/system", "/system", "");')
-  info.script.AppendExtra('rename("/system/vendor/lib64/libril-qc-qmi-1.so.dtwl", "/system/vendor/lib64/libril-qc-qmi-1.so");')
-  info.script.AppendExtra('unmount("/system");')
-  info.script.AppendExtra('endif;')
+  info.script.Mount("/system")
+  info.script.AppendExtra('assert(run_program("/tmp/install/bin/variant_script.sh") == 0);')
+  info.script.Unmount("/system")
