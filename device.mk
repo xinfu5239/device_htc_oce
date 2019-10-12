df9c9e353da4c6cfd55ca21605ccba30aa9a83df
Fagyi <fagyipda@gmail.com>
2019-10-12 22:23:37 +0200

    oce: switch to 1.2 powerhal

--- device.mk
+++ device.mk
@@ -397,7 +397,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    vendor.lineage.power@1.0 \
+    android.hardware.power@1.2-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
