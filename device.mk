9b89602a6efe7169a4affba304a75d7ac8a6ed61
Fagyi <fagyipda@gmail.com>
2019-10-26 17:46:24 +0200

    oce: reenable wfd

--- device.mk
+++ device.mk
@@ -540,8 +540,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libnl
 
-#PRODUCT_BOOT_JARS += \
-#    WfdCommon
+PRODUCT_BOOT_JARS += \
+    WfdCommon
 
 # Wifi tether
 PRODUCT_PROPERTY_OVERRIDES += \
