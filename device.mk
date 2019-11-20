e50f09c0eea3585b9ae3b807419fe2d0deeefe52
TARKZiM <tom8476oo@gmail.com>
2019-11-20 10:05:10 +0800

    oce: Re-enable RR Overlays

--- device.mk
+++ device.mk
@@ -23,8 +23,8 @@ $(call inherit-product-if-exists, vendor/htc/oce/oce-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-#    $(LOCAL_PATH)/overlay-rr
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-rr
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay-lineage/lineage-sdk
