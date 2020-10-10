5a38254e5a2294e76113f45ea3fa31f59d5e1612
TARKZiM <tom8476oo@gmail.com>
2020-10-11 01:48:01 +0800

    oce: Disable traced service

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -215,6 +215,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.delta_time.enable=true
 
+# Traced
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.traced.enable=0
+
 # WFD
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1 \
