e9a4cbf533729e7240cdbf44e1d45bd2101e4d27
xinfu5239 <79011863+xinfu5239@users.noreply.github.com>
2021-02-14 21:31:02 +0800

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -25,9 +25,9 @@ VENDOR=htc
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+AOSP_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/rr/build/tools/extract_utils.sh
+HELPER="$AOSP_ROOT"/vendor/aosp/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
